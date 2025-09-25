51e4a66af138cbacc2a6b1172812538c33ae01fa
Farizgeloz <farizgeloz26@gmail.com>
2025-09-25 12:02:47 +0700

    update commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/* 'use strict';
 
 const Server = require('./src/server');
 
@@ -28,4 +28,40 @@ Server()
 
         console.log(err);
         process.exit(1);
-    });
+    }); */
+
+'use strict';
+
+const Server = require('./src/server');
+
+let _server; // cache biar tidak bikin server baru tiap request
+
+module.exports = async (req, res) => {
+    try {
+        if (!_server) {
+            _server = await Server();
+            await _server.initialize(); // jangan .start(), karena serverless tidak boleh listen port
+        }
+
+        // Panggil Hapi untuk handle request
+        const response = await _server.inject({
+            method: req.method,
+            url: req.url,
+            headers: req.headers,
+            payload: req.body
+        });
+
+        res.status(response.statusCode);
+        for (const [key, value] of Object.entries(response.headers)) {
+            res.setHeader(key, value);
+        }
+
+        res.end(response.rawPayload);
+
+    }
+    catch (err) {
+        console.error('🔥 Serverless error:', err);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
